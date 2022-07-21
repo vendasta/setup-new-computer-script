44f7b92aaab7ce02c7b1ebc63c179df7fae5eb2d
Joel Kesler <joelkesler@gmail.com>
2022-07-21 17:36:32 -0600

    feat: path tweaks

--- setup-new-computer.sh
+++ setup-new-computer.sh
@@ -82,7 +82,7 @@ printError() {
 
 printStep() {
     printf %"$COLUMNS"s |tr " " "-"
-    printf "\nInstalling $1...\n\n";
+    printf "\nInstalling $1...\n";
     $2 || printError "$1"
 }
 
@@ -356,10 +356,14 @@ printDivider
 # Install Brew
 printHeading "Installing Homebrew"
 printDivider
-    echo "✔ Setting Path for Homebrew"
-        export "PATH=${BREW_PREFIX}/bin:${BREW_PREFIX}/sbin:$PATH"
-printDivider
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+printDivider
+    echo "✔ Setting Path for Homebrew"
+    echo "Path Before"
+    echo $PATH
+        export "PATH=${BREW_PREFIX}/bin:${BREW_PREFIX}/sbin:$PATH"
+    echo "Path After"
+    echo $PATH
 printDivider
     echo "✔ (zsh) Fix brew insecure directories warning"
     chmod go-w "$(brew --prefix)/share"
